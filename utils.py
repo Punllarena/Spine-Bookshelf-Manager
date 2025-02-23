395974f62827f1ede83e4492f11d0b3c80b24ca4
Pun <puncedy@gmail.com>
2025-02-23 16:28:40 +0800

    Update utils.py
    Utils.py
    Added a few more items to the replace string for better search results in Nyaa
    
    volumeinfo.html
    anchor tags to the trick I used in search bars.

--- utils.py
+++ utils.py
@@ -3,9 +3,14 @@ import re
 def clean_for_search_title(title):
     replacements = {
         ":": "",
+        ",": "",
+        ".": "",
+        "-": "  ",
         "(volume)": "",
+        "Volume": "",
         "volume": "",
         "vol": "",
+        "Vol":"",
         "act": "",
         "(manga)": "", 
         "manga": "",
